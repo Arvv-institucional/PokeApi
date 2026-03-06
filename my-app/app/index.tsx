531bf318b956549a400583f93e8b2d21a553afde
Arvv_institucional <up240589@upa.edu.mx>
2026-03-06 15:40:48 -0600

    sistema de busqueda

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,6 +1,6 @@
 
 import React, { useEffect, useState } from "react"
-import { ScrollView, Text, View } from "react-native"
+import { ScrollView, Text, View, TextInput, StyleSheet } from "react-native"
 import PokemonCard from "@/components/PokemonCard"
 
 interface Pokemon {
@@ -17,8 +17,9 @@ interface PokemonListItem {
 }
 
 export default function Index() {
+  const[pokemonFinder, setPokemonFinder] = useState<String>("")
   const[pokemonData, setPokemonData] = useState<Pokemon[]>([])
-  const pokemon_URL = 'https://pokeapi.co/api/v2/pokemon?limit=20'
+  const pokemon_URL = 'https://pokeapi.co/api/v2/pokemon?limit=150'
   const fetchPokemon = async () => {
     try {
 
@@ -39,14 +40,36 @@ export default function Index() {
   useEffect(() => {
     fetchPokemon()
   }, [])
+
+  useEffect(() => {
+    fetchPokemon()
+  }, [pokemonFinder])
+
+  const pokemonsfiltrados = pokemonData.filter(pokemon => pokemon.name.toLowerCase().includes(pokemonFinder.toLowerCase()))
+  
+
+  useEffect
   return ( 
 
     <ScrollView>
+      <View>
+        <TextInput style={styles.cuadroText}
+        placeholder="Buscar pokemon por nombre"
+        onChangeText={(input) => setPokemonFinder(input)}>
+        </TextInput>
+      </View>
       <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center', padding: 10 }}>
-        {pokemonData.map((pokemon,) => (
+        {pokemonsfiltrados.map((pokemon,) => (
           <PokemonCard key={pokemon.id} item={pokemon}/>
         ))}
       </View>
     </ScrollView>
   );
 }
+
+const styles = StyleSheet.create ({
+  cuadroText : {
+    backgroundColor : "white",
+    borderRadius : 20,
+    padding : 10}
+  })
